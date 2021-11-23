2dd5c5f939d011b8d73bdede0d534d79a867b345
Romeo di Gregorio <r.digregorio@crif.com>
2021-11-23 18:21:04 +0100

    Add project files.

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public string? MyName { get; set; }
+        public string Response { get; set; } = "Nothing received so far ...";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetMessage();
+        }
+
+        private async Task GetMessage()
+        {
+            Response = await HttpClient.GetStringAsync(Constants.API_DEFAULT_URL + $"?name={MyName}");
+        }
+    }
+}
