bdfc0540075de2233f3b63f759aa31be3649c8be
Romeo di Gregorio <r.digregorio@crif.com>
2021-11-27 16:23:49 +0100

    Change BaseAddress for HttpClient from localhost to HostEnvironment.BaseAddress

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,13 +9,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Localhost", client =>
-{
-    client.BaseAddress = new Uri(ApiRoutes.LOCALHOST_DEFAULT_URL);
-});
-
 //builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>()!.CreateClient("Localhost"));
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ApiRoutes.LOCALHOST_DEFAULT_URL) });
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ApiRoutes.LOCALHOST_DEFAULT_URL) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ITodoHttpRepository, TodoHttpRepository>();
 
