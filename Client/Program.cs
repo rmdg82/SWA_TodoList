d51c489ac79a1188874d741c0337fbc4148d8256
Romeo di Gregorio <r.digregorio@crif.com>
2022-01-01 17:43:53 +0100

    Add environment checking before HttpClient creation

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,8 +9,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ApiRoutes.LOCALHOST_DEFAULT_URL) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+if (builder.HostEnvironment.IsDevelopment() || builder.HostEnvironment.IsStaging())
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ApiRoutes.LOCALHOST_DEFAULT_URL) });
+}
+else
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+}
 
 builder.Services.AddScoped<ITodoHttpRepository, TodoHttpRepository>();
 
