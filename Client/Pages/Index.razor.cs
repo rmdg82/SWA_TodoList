180eec7085b29d65f3d9899275fc59a2dd0bcfe9
Romeo di Gregorio <r.digregorio@crif.com>
2022-01-11 11:43:10 +0100

    Add fe check for max length (allowed 20 chars) on new todo text

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -9,17 +9,19 @@ namespace Client.Pages
     public partial class Index
     {
         [Inject]
-        public IWebAssemblyHostEnvironment HostEnvironment { get; set; }
+        public IWebAssemblyHostEnvironment? HostEnvironment { get; set; }
 
         [Inject]
-        public IDialogService DialogService { get; set; }
+        public IDialogService? DialogService { get; set; }
 
         [Inject]
-        public ITodoHttpRepository TodoHttpRepository { get; set; }
+        public ITodoHttpRepository? TodoHttpRepository { get; set; }
 
-        public IEnumerable<TodoDto> AllTodos { get; set; }
+        public IEnumerable<TodoDto>? AllTodos { get; set; }
 
         public string NewTodoText { get; set; } = string.Empty;
+
+        private Func<string, string?> ValidationFunc { get; set; } = CheckMaxCharacters;
 
         protected override async Task OnInitializedAsync()
         {
@@ -28,33 +30,39 @@ namespace Client.Pages
 
         public async Task AddTodo()
         {
+            if (CheckMaxCharacters(NewTodoText) is not null)
+            {
+                await DialogService!.ShowMessageBox("Error", "The todo size cannot exceed 20 characters long.");
+                return;
+            }
+
             var todoToAdd = new TodoDtoToAdd(NewTodoText);
 
-            await TodoHttpRepository.AddTodo(todoToAdd);
+            await TodoHttpRepository!.AddTodo(todoToAdd);
             await LoadAllTodos();
             NewTodoText = string.Empty;
         }
 
         public async Task CompleteTodo(string todoId)
         {
-            await TodoHttpRepository.CompleteTodo(todoId);
+            await TodoHttpRepository!.CompleteTodo(todoId);
             await LoadAllTodos();
         }
 
         public async Task ShowMessage()
         {
-            await DialogService.ShowMessageBox("Not implemented", "Method not implemented yet!");
+            await DialogService!.ShowMessageBox("Not implemented", "Method not implemented yet!");
         }
 
         public async Task DeleteTodo(string todoId)
         {
-            await TodoHttpRepository.DeleteTodo(todoId);
+            await TodoHttpRepository!.DeleteTodo(todoId);
             await LoadAllTodos();
         }
 
         public async Task ResetDb()
         {
-            await TodoHttpRepository.ResetDb();
+            await TodoHttpRepository!.ResetDb();
             AllTodos = await TodoHttpRepository.GetTodos();
 
             StateHasChanged();
@@ -62,8 +70,18 @@ namespace Client.Pages
 
         private async Task LoadAllTodos()
         {
-            AllTodos = await TodoHttpRepository.GetTodos();
+            AllTodos = await TodoHttpRepository!.GetTodos();
             StateHasChanged();
+        }
+
+        private static string? CheckMaxCharacters(string ch)
+        {
+            if (!string.IsNullOrEmpty(ch) && ch.Length > 25)
+            {
+                return "Max 20 characters allowed!";
+            }
+
+            return null;
         }
     }
 }
