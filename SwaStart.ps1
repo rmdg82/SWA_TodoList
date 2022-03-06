d1b2f6fc0b91cc287ad4dd052fdd0ce9f0eddea6
Romeo di Gregorio <r.digregorio@crif.com>
2022-03-06 06:01:53 +0100

    Add starting script for SWA CLI

---
+++ SwaStart.ps1
@@ -0,0 +1,21 @@
+﻿# Script to start SWA CLI in order to test authentication/authorization
+
+$SwaUrl = "http://localhost:5260"
+
+# Go to project
+Set-Location "C:\Users\esz42romdigr\source\repos\rmdg82\SWA_TodoList"
+
+# Start swa
+swa start $SwaUrl --api-location .\Api &
+
+# Start fe
+dotnet run watch --project .\Client\Client.csproj &
+
+# Start be
+#func host start --script-root .\Api\ &
+
+# Sleep 3 sec
+Start-Sleep 3
+
+# Lauch browser
+Start-Process microsoft-edge:$SwaUrl
