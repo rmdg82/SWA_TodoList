bdbdfe7ccbaefee3668cf6cea883ec8f124f1d67
Romeo di Gregorio <r.digregorio@crif.com>
2022-03-09 12:28:10 +0100

    Smal fix

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -41,7 +41,7 @@ public partial class Index
     public async Task GetIdentity()
     {
         var client = new HttpClient();
-        var response = await client.GetFromJsonAsync<IdentityDto>(".auth/me");
+        var response = await client.GetFromJsonAsync<IdentityDto>("/.auth/me");
         //identity = JsonSerializer.Serialize(response);
         identity = response.ClientPrincipal.UserId;
         //identity = await response.Content.ReadAsStringAsync();
