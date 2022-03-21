4bc3e0f6a7dddf0e02623b0ec8f6e1a3d9c28d42
Romeo di Gregorio <r.digregorio@crif.com>
2022-03-21 23:48:05 +0100

    First draft for login page

---
+++ Client/Pages/Login.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public partial class Login
+    {
+        public static string[] Providers { get; } = { "github", "twitter", "aad" };
+
+        public static string GetProviderLink(string provider) => $"/.auth/login/{provider}";
+    }
+}
---
+++ Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,24 @@
+﻿using Client.HttpRepository;
+using Microsoft.AspNetCore.Components;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        public IAuthRepository? AuthRepository { get; set; }
+
+        public IdentityDto? IdentityDto { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            IdentityDto = await AuthRepository!.GetIdentity();
+        }
+    }
+}
