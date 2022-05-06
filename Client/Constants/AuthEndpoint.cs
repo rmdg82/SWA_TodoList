4c198b8eef78b394b8fe50b3d873b41bf91ffa64
Romeo di Gregorio <r.digregorio@crif.com>
2022-05-06 15:47:28 +0200

    Add constants for Authentication endpoints

---
+++ Client/Constants/AuthEndpoint.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Constants;
+
+public static class AuthEndpoint
+{
+    public const string Me = "/.auth/me";
+
+    public const string Logout = "/.auth/logout";
+
+    public static class LoginProviders
+    {
+        public const string GitHub = "/.auth/login/github";
+        public const string Microsoft = "/.auth/login/aad";
+        public const string Twitter = "/.auth/login/twitter";
+    }
+}
